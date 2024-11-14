db428b72a16292ec5c657949c2fadc1da63b6457
D. Tuinstra <dtuinstr@clarkson.edu>
2024-11-14 10:00:41 -0500

    Implement all non-abstract methods.

--- src/clack/cipher/CharacterCipher.java
+++ src/clack/cipher/CharacterCipher.java
@@ -21,6 +21,25 @@ public abstract class CharacterCipher {
     }
 
     /**
+     * Mathematical "mod" operator. Use instead of Java's "%"
+     * operator when shifting leftward (a negative shift), as
+     * this will always return a number in the range [0, modulus).
+     *
+     * @param n the number to be "modded".
+     * @param modulus the modulus.
+     * @throws IllegalArgumentException if modulus < 1.
+     */
+    public static int mod(int n, int modulus) {
+        if (modulus < 1) {
+            throw new IllegalArgumentException("modulus cannot be < 1");
+        }
+        // n % modulus -> in range [-modulus, modulus)
+        // (n % modulus) + modulus -> in [0, 2 * modulus)
+        // full expression -> in [0, modulus)
+        return ((n % modulus) + modulus) % modulus
+    }
+
+    /**
      * Returns the character that is n letters further on in ALPHABET,
      * with wrap around at either end of ALPHABET. Negative values are
      * allowed and cause a shift to the left. A shift of 0 returns
@@ -35,8 +54,8 @@ public abstract class CharacterCipher {
             throw new IllegalArgumentException(
                     "Argument ('" + c + "') not in ALPHABET");
         }
-        // TODO implement remainder of this.
-        return ' ';
+        int charPos = ALPHABET.indexOf(c);
+        return ALPHABET.charAt(mod(charPos + n, ALPHABET.length()));
     }
 
     /**
@@ -50,14 +69,17 @@ public abstract class CharacterCipher {
      *      is not in ALPHABET.
      */
     public static String shift(String str, int n) {
-        // TODO implement this.
-        return "";
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < str.length(); ++i) {
+            chars[i] = shift(chars[i], n);
+        }
+        return new String(chars);
     }
 
     /**
      * Prepare cleartext for encrypting. At minimum this requires
      * removing spaces, punctuation, and non-alphabetic characters,
-     * then uppercasing what's left. Other cipers, such as PLAYFAIR,
+     * then uppercasing what's left. Other ciphers, such as PLAYFAIR,
      * may have additional preparation that this method needs to do.
      * @param cleartext
      * @return a version of the cleartext ready for encrypting.
