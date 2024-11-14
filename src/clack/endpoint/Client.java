c863cc86b168d71c8ec3346c238a5f98a57f5411
D. Tuinstra <dtuinstr@clarkson.edu>
2024-11-14 17:29:53 -0500

    Implemented client.

--- src/clack/endpoint/Client.java
+++ src/clack/endpoint/Client.java
@@ -1,6 +1,7 @@
 package clack.endpoint;
 
 import clack.message.*;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -39,8 +40,7 @@ public class Client {
      * @param username username to include in Messages.
      * @throws IllegalArgumentException if port not in range [1-49151]
      */
-    public Client(String hostname, int port, String username)
-    {
+    public Client(String hostname, int port, String username) {
         if (port < 1 || port > 49151) {
             throw new IllegalArgumentException(
                     "Port " + port + " not in range 1 - 49151.");
@@ -59,8 +59,7 @@ public class Client {
      * @param port     the service's port on the server.
      * @throws IllegalArgumentException if port not in range [1-49151]
      */
-    public Client(String hostname, int port)
-    {
+    public Client(String hostname, int port) {
         this(hostname, port, DEFAULT_USERNAME);
     }
 
@@ -71,18 +70,15 @@ public class Client {
      * @throws UnknownHostException if hostname is not resolvable.
      * @throws IOException          if socket creation, wrapping, or IO fails.
      */
-    public void start() throws UnknownHostException, IOException, ClassNotFoundException
-    {
+    public void start() throws UnknownHostException, IOException, ClassNotFoundException {
         System.out.println("Attempting connection to " + hostname + ":" + port);
         Scanner keyboard = new Scanner(System.in);
 
         try (
-            // TODO Create a socket (named 'socket') using hostname and port.
-
-            // TODO Construct an ObjectInputStream and ObjectOutputStream on the socket.
-            // TODO Name them 'inObj' and 'outObj'.
-        )
-        {
+                Socket socket = new Socket(hostname, port);
+                ObjectInputStream inObj = new ObjectInputStream(socket.getInputStream());
+                ObjectOutputStream outObj = new ObjectOutputStream(socket.getOutputStream());
+        ) {
             String userInput;
             Message inMsg;
             Message outMsg;
@@ -91,30 +87,89 @@ public class Client {
             do {
                 // Get server message and show it to user.
                 inMsg = (Message) inObj.readObject();
-                // TODO use a switch statement or expression, based on MsgType,
-                // TODO to decide what to show the user.
+                System.out.println(switch (inMsg.getMsgType()) {
+                    case FILE -> "FILE message received:" + inMsg;
+                    case TEXT -> ((TextMessage) inMsg).getText();
+                    default -> "UNEXPECTED MESSAGE: " + inMsg;
+                });
 
                 // Get user input
-                System.out.print(prompt);
-                userInput = keyboard.nextLine();
-                String[] tokens = userInput.trim().split("\\s+");
+                String[] tokens;
+                do {
+                    System.out.print(prompt);
+                    userInput = keyboard.nextLine();
+                    tokens = userInput.trim().split("\\s+");
+                } while (tokens.length == 0);
                 // DEBUG
                 // System.out.println("tokens: " + Arrays.toString(tokens));
 
                 // Construct Message based on user input and send it to server.
-                // TODO use a switch statement or expression, based on MsgType,
-                // TODO to decide what Message object to construct.
-
-                // TODO send it to the server.
-            } while (outMsg.getMsgType() != MsgType.LOGOUT);
+                switch (tokens[0].toUpperCase()) {
+                    case "SEND" -> {
+                        if (tokens.length == 3
+                                && tokens[1].equalsIgnoreCase("FILE")) {
+                            outMsg = new FileMessage(username, tokens[2], tokens[2]);
+                        } else if (tokens.length == 5
+                                && tokens[1].equalsIgnoreCase("FILE")
+                                && tokens[3].equalsIgnoreCase("AS")) {
+                            outMsg = new FileMessage(username, tokens[2], tokens[4]);
+                        } else {
+                            outMsg = new TextMessage(username, userInput);
+                        }
+                    }
+                    case "HELP" -> outMsg = new HelpMessage(username);
+                    case "LIST" -> {
+                        if (tokens.length > 1
+                                && tokens[1].equalsIgnoreCase("USERS")) {
+                            outMsg = new ListUsersMessage(username);
+                        } else {
+                            outMsg = new TextMessage(username, userInput);
+                        }
+                    }
+                    case "LOGIN" -> {
+                        if (tokens.length > 1) {
+                            outMsg = new LoginMessage(username, tokens[1]);
+                        } else {
+                            outMsg = new TextMessage(username, userInput);
+                        }
+                    }
+                    case "LOGOUT" -> outMsg = new LogoutMessage(username);
+                    case "OPTION" -> {
+                        if (tokens.length == 3) {
+                            // What option is being set/queried?
+                            OptionEnum option = switch (
+                                    tokens[1].toUpperCase()) {
+                                case "CIPHER_KEY" -> OptionEnum.CIPHER_KEY;
+                                case "CIPHER_NAME" -> OptionEnum.CIPHER_NAME;
+                                case "CIPHER_ENABLE" -> OptionEnum.CIPHER_ENABLE;
+                                default -> null;    // Not a valid option.
+                            };
+                            // Build appropriate message for that option.
+                            if (option != null) {
+                                outMsg = new OptionMessage(username,
+                                        option, tokens[2]);
+                            } else {    // Not a valid option.
+                                outMsg = new TextMessage(username, userInput);
+                            }
+                        } else {    // Not a valid OptionMessage
+                            outMsg = new TextMessage(username, userInput);
+                        }
+                    }
+                    default ->
+                            // unrecognized token
+                            outMsg = new TextMessage(username, userInput);
+                }
+                outObj.writeObject(outMsg);
+                outObj.flush();
+            } while (outMsg.getMsgType() != MsgTypeEnum.LOGOUT);
 
             // Get server's closing reply and show it to user.
             inMsg = (Message) inObj.readObject();
             System.out.println(
                     switch (inMsg.getMsgType()) {
-                        case LISTUSERS -> "UNEXPECTED RESPONSE: " + inMsg;
-                        case LOGOUT -> "UNEXPECTED RESPONSE: " + inMsg;
                         case TEXT -> ((TextMessage) inMsg).getText();
+                        default -> "UNEXPECTED CLOSING MESSAGE: "
+                                + inMsg;
                     });
         }   // Streams and sockets closed by try-with-resources
 
