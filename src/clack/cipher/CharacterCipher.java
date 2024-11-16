4c6fb62ebfbd5532ceda9ad21165eafbe44ebe00
D. Tuinstra <dtuinstr@clarkson.edu>
2024-11-16 13:27:50 -0500

    Implement CharacterCipherTest, bug fix in CharacterCipher.group(), handle null strings in CharacterCipher methods.

--- src/clack/cipher/CharacterCipher.java
+++ src/clack/cipher/CharacterCipher.java
@@ -7,14 +7,32 @@ public abstract class CharacterCipher {
     public static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     /**
+     * Removes all non-alphabet characters from a string, and
+     * uppercases all remaining letters. This is a utility
+     * method, useful in implementing prep(). If the argument
+     * is null, returns null.
+     *
+     * @param str the string to clean
+     * @return the cleaned string (which might be empty), or null.
+     */
+    public static String clean(String str) {
+        if (str == null) {
+            return null;
+        }
+        return str.trim().toUpperCase().replaceAll("[^A-Z]", "");
+    }
+
+    /**
      * Return a copy of a string, but reformatted into groups of
-     * <em>n</em> non-whitespace characters, with groups separated
+     * n non-whitespace characters, with groups separated
      * by a space. The last group may have fewer than <em>n</em>
-     * characters.
+     * characters. If n < 1, throws IllegalArgumentException;
+     * if str is null, returns null. Does not alter the string in
+     * any other way.
      *
      * @param str the string to break into groups
      * @param n   how many characters in each group
-     * @return the grouped version of the argument string
+     * @return the grouped version of the argument string, or null.
      * @throws IllegalArgumentException if n < 1
      */
     public static String group(String str, int n) {
@@ -22,8 +40,17 @@ public abstract class CharacterCipher {
             throw new IllegalArgumentException(
                     "groups must have 1 or more letters");
         }
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        // We have a non-null, non-empty string. Group it.
         String regex = "(" + ".".repeat(n) + ")";
-        return str.replaceAll(regex, "$1 ");
+        String result = str.replaceAll(regex, "$1 ");
+        if (str.length() % n == 0) {
+            // ... we put an extra blank space at the end of result.
+            result = result.substring(0, result.length() - 1);
+        }
+        return result;
     }
 
     /**
@@ -39,9 +66,9 @@ public abstract class CharacterCipher {
         if (modulus < 1) {
             throw new IllegalArgumentException("modulus cannot be < 1");
         }
-        // n % modulus -> in range [-modulus, modulus)
-        // (n % modulus) + modulus -> in [0, 2 * modulus)
-        // full expression -> in [0, modulus)
+        // n % modulus -> in range (-modulus, modulus)
+        // (n % modulus) + modulus -> in (0, 2 * modulus)
+        // ((n % modulus) + modulus) % modulus -> in [0, modulus)
         return ((n % modulus) + modulus) % modulus;
     }
 
@@ -68,15 +95,19 @@ public abstract class CharacterCipher {
     /**
      * Returns the string resulting from shifting each character of str
      * by n places, (positive to right, negative to left), with wrap
-     * around at either end of ALPHABET.
+     * around at either end of ALPHABET. If the string argument is null,
+     * returns null.
      *
      * @param str the string to shift.
      * @param n   the amount to shift each letter.
-     * @return the shifted version of str.
+     * @return the shifted version of str, or null if str is null.
      * @throws IllegalArgumentException if any character in String
      *                                  is not in ALPHABET.
      */
     public static String shift(String str, int n) {
+        if (str == null) {
+            return null;
+        }
         char[] chars = str.toCharArray();
         for (int i = 0; i < str.length(); ++i) {
             chars[i] = shift(chars[i], n);
