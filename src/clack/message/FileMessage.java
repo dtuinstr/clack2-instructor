eb4305e5f0bca26c3165125952285c8229291ac1
D. Tuinstra <dtuinstr@clarkson.edu>
2024-11-15 22:27:40 -0500

    FileMessageTest, minor fixes to FileMessage.

--- src/clack/message/FileMessage.java
+++ src/clack/message/FileMessage.java
@@ -7,14 +7,14 @@ import java.nio.file.Path;
 /**
  * Message class that carries the contents of a file, and a suggested
  * name to use when saving it on another user's device.
- *
+ * <p>
  * This class does not override equals() and hashCode(). Given the
  * precision of the timestamp it is unlikely that any two objects of
  * any Message subclass will ever test equal, or have equal hashCodes.
  */
 public class FileMessage extends Message {
-    private String fileContents;
-    private String fileName;
+    private final String fileContents;
+    private final String fileName;
 
     /**
      * Constructs a new FileMessage object by reading the contents of
@@ -30,6 +30,9 @@ public class FileMessage extends Message {
     public FileMessage(String username, String fileReadPath,
                        String fileSaveAsName) throws IOException {
         super(username, MsgTypeEnum.FILE);
+        // readString() "... ensures that the file is closed when all
+        // content have been read or an I/O error, or other runtime
+        // exception, is thrown." -- File.readString() javadoc.
         fileContents = Files.readString(Path.of(fileReadPath));
         fileName = String.valueOf(
                 Path.of(fileSaveAsName).getFileName());
